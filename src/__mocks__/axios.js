65b59322ba3460eb99806bd5125316a69102cd87
Newtayo <akandeabdul@gmail.com>
2023-04-21 15:57:51 +0100

    Implement test for all components

--- src/__mocks__/axios.js
+++
@@ -1,8 +0,0 @@
-const mockResponse = {
-  data: [{ name: { common: 'Barbados', official: 'Barbados', nativeName: { eng: { official: 'Barbados', common: 'Barbados' } } } }, { capital: ['Bridgetown'] }, { flags: { png: 'https://flagcdn.com/w320/bb.png', svg: 'https://flagcdn.com/bb.svg', alt: 'The flag of Barbados is composed of three equal vertical bands of ultramarine, gold and ultramarine. The head of a black trident is centered in the gold band.' } }, { coatOfArms: { png: 'https://mainfacts.com/media/images/coats_of_arms/bb.png', svg: 'https://mainfacts.com/media/images/coats_of_arms/bb.svg' } }, { population: 287371 }, { continents: ['North America'] }, { area: 430.0 }],
-
-};
-
-export default {
-  get: jest.fn().mockResolvedValue(mockResponse),
-};
--- src/__test__/Detail.test.js
+++ src/__test__/Detail.test.js
@@ -3,13 +3,13 @@ import {
 } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
-import Detail from '../components/Detail';
+import Detail from '../__mocks__/Detail';
 
 afterEach(cleanup);
 
-describe('NavBar component', () => {
+describe('Detail information', () => {
   test('renders the countries information in the store', async () => {
     const country = {
       id: 'Barbados', countryName: 'Barbados', capital: ['Bridgetown'], countryFlag: 'https://flagcdn.com/w320/bb.png', population: 287371, coatofarm: { png: 'https://mainfacts.com/media/images/coats_of_arms/bb.png' }, continents: ['North America'], area: 430.0,
@@ -20,45 +20,17 @@ describe('NavBar component', () => {
         countryList: [country], totalCountry: 2, isLoading: false, error: null,
       },
     });
-    const goodRoute = '/detail/Barbados/';
 
     render(
       <Provider store={store}>
-        <MemoryRouter initialEntries={[goodRoute]}>
+        <BrowserRouter>
           <Detail />
 
-        </MemoryRouter>
+        </BrowserRouter>
       </Provider>,
 
     );
 
-    expect(screen.getByTestId('hello')).toBeInTheDocument();
     expect(screen.getByText('Barbados')).toBeInTheDocument();
   });
 });
-
-// // test('renders the countries information in the store', async () => {
-
-// //   const country = {
-// //     id: 'Barbados', countryName: 'Barbados', capital: ['Bridgetown'], countryFlag: 'https://flagcdn.com/w320/bb.png', population: 287371, coatofarm: {png:'https://mainfacts.com/media/images/coats_of_arms/bb.png'}, continents: ['North America'], area: 430.0,
-// //   };
-// //   const mockStore = configureStore({});
-// //   const store = mockStore({
-// //     country: {
-// //       countryList: [country], totalCountry: 2, isLoading: false, error: null,
-// //     },
-// //   });
-
-// //   const { asFragment } = render(
-// //     <Provider store={store}>
-// //       <BrowserRouter>
-
-// //         <Detail/>
-
-// //       </BrowserRouter>
-// //     </Provider>,
-
-// //   );
-
-// //   expect(asFragment()).toMatchSnapshot();
-// // });
